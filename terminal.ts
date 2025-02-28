0e2af9dca61e8b8eb2e9833e82bb808444e3f710
Dylan <dylan.de.brouwer@outlook.com>
2025-02-28 08:33:46 +0000

    terminal

---
+++ terminal.ts
@@ -0,0 +1,27 @@
+import * as readline from 'readline-sync';
+import teamsPlayers from './teams&players.json';
+import playerStats from './playerStats.json';
+
+const option: number = readline.questionInt("Welcome to the JSON data viewer!\n\n1. View all data\n2. Filter by ID\n3. Exit\n\nPlease enter your choice: ");
+
+switch (option) {
+    case 1:
+        console.log("All Data:");
+        console.log(JSON.stringify(teamsPlayers, null, 2));
+        console.log(JSON.stringify(playerStats, null, 2));
+        break;
+    case 2:
+        const choice: number = readline.questionInt("Please enter the ID you want to filter by: ");
+        const filteredTeamsPlayers = teamsPlayers.filter((item: any) => item.id === choice);
+        const filteredPlayerStats = playerStats.filter((item: any) => item.id === choice);
+        console.log("Filtered Data by ID:");
+        console.log(JSON.stringify(filteredTeamsPlayers, null, 2));
+        console.log(JSON.stringify(filteredPlayerStats, null, 2));
+        break;
+    case 3:
+        console.log("Exiting...");
+        break;
+    default:
+        console.log("Invalid option. Please enter a valid choice.");
+        break;
+}
